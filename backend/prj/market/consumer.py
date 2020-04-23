360cea1f182e05fac21206bdfce621aa2c57c66c
Dmitry Zharikov <zdimon77@gmail.com>
2020-04-23 15:41:56 +0300

    commit from zdimon77@gmail.com --channels-install--

---
+++ backend/prj/market/consumer.py
@@ -0,0 +1,21 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+class MarketConsumer(WebsocketConsumer):
+
+    def connect(self):
+        self.accept()
+        self.send(text_data=json.dumps({"message_from_server": "hello"}))
+
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        print(text_data_json)
+        # message = text_data_json['message']
+
+        # self.send(text_data=json.dumps({
+        #     'message': message
+        # }))
--- backend/prj/prj/settings.py
+++ backend/prj/prj/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'market',
-    'drf_yasg'
+    'drf_yasg',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'prj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +124,4 @@ STATIC_URL = '/static/'
 
 DATA_DIR = os.path.join(BASE_DIR,'../init_data')
 
+ASGI_APPLICATION = "market.ws_route_app.application"
