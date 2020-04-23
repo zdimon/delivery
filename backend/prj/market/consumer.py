036048e533e7baa7d2ecc8ab5316cba36a5b4ffd
Dmitry Zharikov <zdimon77@gmail.com>
2020-04-23 16:05:34 +0300

    commit from zdimon77@gmail.com ----

--- backend/prj/market/consumer.py
+++ backend/prj/market/consumer.py
@@ -14,8 +14,3 @@ class MarketConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         print(text_data_json)
-        # message = text_data_json['message']
-
-        # self.send(text_data=json.dumps({
-        #     'message': message
-        # }))
